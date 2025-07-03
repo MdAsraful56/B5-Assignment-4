9b2642015189bfd85028ed8ed72abd46c040c40b
MdAsraful56 <work.mdasraful56@gmail.com>
2025-07-03 18:59:58 +0600

    create bookApi

---
+++ src/redux/store.ts
@@ -0,0 +1,20 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { bookApi } from "./api/books";
+
+export const store = configureStore({
+  reducer: {
+    [bookApi.reducerPath]: bookApi.reducer,
+  },
+
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(bookApi.middleware);
+  },
+});
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
+
+setupListeners(store.dispatch);
